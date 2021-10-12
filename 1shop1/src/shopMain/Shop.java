3e4d6b5f92758e3ee50a6382627c190a04f6e150
gujuhyeong <gujuhyeong@192.168.219.101>
2021-10-12 20:16:05 +0900

    shop프로젝

---
+++ 1shop1/src/shopMain/Shop.java
@@ -0,0 +1,16 @@
+package shopMain;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Shop {
+	public static Scanner sc = new Scanner(System.in);
+	public static Random rn = new Random();
+	
+	public void mainMenu() {
+		System.out.println("[1.가입] [2.탈퇴] [3.로그인] [4.로그아웃]");
+	}
+
+	
+
+}
