179d32bc830646c44569006f1ce18acc408bf5b7
gujuhyeong <gujuhyeong@192.168.219.101>
2021-11-02 16:09:09 +0900

    완료

--- Lv10Test_Poly_구주형/src/controllerPoly/Main.java
+++ Lv10Test_Poly_구주형/src/controllerPoly/Main.java
@@ -1,7 +1,7 @@
 package controllerPoly;
-//시작 : 00:00
-//종료 : 00:00
-//소요 : 00:00
+//시작 : 18:10
+//종료 : 03:00
+//소요 : 09:00
 public class Main {
 	public static void main(String[] args) {
 		GameManager gm = GameManager.getInstance();
